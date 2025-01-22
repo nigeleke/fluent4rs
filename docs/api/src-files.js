62289a11ca0d1841d495572a4a8abb3e9e99b2cb
Nigel Eke <nigeleke@users.noreply.github.com>
2025-01-22 11:42:20 +0000

    Release fluent4rs@0.1.1

---
+++ docs/api/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["fluent4rs",["",[["ast",[],["argument.rs","attribute.rs","attribute_accessor.rs","block_placeable.rs","block_text.rs","call_arguments.rs","comment_line.rs","default_variant.rs","entry.rs","function_reference.rs","identifier.rs","inline_expression.rs","inline_placeable.rs","inline_text.rs","junk.rs","literal.rs","message.rs","message_reference.rs","mod.rs","named_argument.rs","number_literal.rs","pattern.rs","pattern_element.rs","resource.rs","select_expression.rs","string_literal.rs","term.rs","term_reference.rs","variable_reference.rs","variant.rs","variant_key.rs","variant_list.rs"]]],["error.rs","grammar.rs","lib.rs","parser.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[643]}
