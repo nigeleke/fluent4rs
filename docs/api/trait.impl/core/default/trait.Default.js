cb8e2d1e02b615752fb9caaee66bf32d3e402578
Nigel Eke <nigeleke@users.noreply.github.com>
2025-02-27 23:19:24 +0000

    Release fluent4rs@0.1.41

--- docs/api/trait.impl/core/default/trait.Default.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["fluent4rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"fluent4rs/ast/struct.Resource.html\" title=\"struct fluent4rs::ast::Resource\">Resource</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[291]}
