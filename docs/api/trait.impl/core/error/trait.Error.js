c4aa1107c14778fb1c3c45ae3f2b6d10f7ef9bec
Nigel Eke <nigeleke@users.noreply.github.com>
2025-02-27 13:32:17 +0000

    Release fluent4rs@0.1.40

--- docs/api/trait.impl/core/error/trait.Error.js
+++ docs/api/trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["fluent4rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"fluent4rs/parser/enum.ParserError.html\" title=\"enum fluent4rs::parser::ParserError\">ParserError</a>"]]]]);
+    var implementors = Object.fromEntries([["fluent4rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"fluent4rs/parser/enum.ParserError.html\" title=\"enum fluent4rs::parser::ParserError\">ParserError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
