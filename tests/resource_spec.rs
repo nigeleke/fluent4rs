a1ea02686e9d5ea293c4b08800518c299d1baab4
Nigel Eke <nigeleke@gmail.com>
2026-01-23 14:26:59 +1100

    Resource::from(Vec<Entry>) clippified test

--- tests/resource_spec.rs
+++ tests/resource_spec.rs
@@ -8,7 +8,7 @@ fn resource_can_be_created_from_entries_only() {
     let ftl0 = include_str!("data/full_grammar_example.ftl");
     let ast0 = Parser::parse(ftl0).unwrap();
 
-    let entries = Vec::from_iter(ast0.entries().into_iter().map(|e| e.clone()));
+    let entries = Vec::from_iter(ast0.entries().into_iter().cloned());
     let ast1 = Resource::from(entries);
 
     assert_eq!(ast1.entries(), ast0.entries());
